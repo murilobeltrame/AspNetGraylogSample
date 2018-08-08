b3ebf9d76dcd69590e56117891c2e1034ab038c3
Murilo Beltrame <murilobeltrame@gmail.com>
2018-08-08 01:32:20 -0300

    First commit

---
+++ SampleMVC/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SampleMVC.Models;
+
+namespace SampleMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Somethign informational happend");
+            _logger.LogWarning("Something warmfull happend");
+            _logger.LogError("Bad bad Jimmy");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SampleMVC/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Gelf.Extensions.Logging;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace SampleMVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .ConfigureLogging((context, builder) =>
+                {
+                    // Read GelfLoggerOptions from appsettings.json
+                    builder.Services.Configure<GelfLoggerOptions>(context.Configuration.GetSection("Graylog"));
+
+                    // Optionally configure GelfLoggerOptions further.
+                    builder.Services.PostConfigure<GelfLoggerOptions>(options =>
+                        options.AdditionalFields["machine_name"] = Environment.MachineName);
+
+                    // Read Logging settings from appsettings.json and add providers.
+                    builder.AddConfiguration(context.Configuration.GetSection("Logging"))
+                        .AddConsole()
+                        .AddDebug()
+                        .AddGelf();
+                });
+    }
+}
